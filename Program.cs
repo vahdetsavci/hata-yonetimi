cea21474850e3039423f878b19a7a8a8255f8873
Vahdet Savci <savcivahdet@gmail.com>
2023-10-20 15:42:44 +0300

    project updated

--- Program.cs
+++ Program.cs
@@ -6,20 +6,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        // try
-        // {
-        //     Console.Write("Bir sayı giriniz: ");
-        //     int sayi = Convert.ToInt32(Console.ReadLine());
-        //     Console.WriteLine(sayi);
-        // }
-        // catch (Exception ex)
-        // {
-        //     Console.WriteLine("Hata: " + ex.Message.ToString());
-        // }
-        // finally
-        // {
-        //     Console.Write("İşlem tamamlandı.");
-        // }
+        try
+        {
+            Console.Write("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sayi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message.ToString());
+        }
+        finally
+        {
+            Console.Write("İşlem tamamlandı.");
+        }
 
         try
         {
